bc09d60921c0e4c0c3435c60ae2b29881f633878
Angelo van Meurs <arcvanmeurs@hotmail.com>
2023-04-12 17:09:04 +0200

    Separate Matrix, Maze and Validator; Write more tests

--- solve.py
+++ solve.py
@@ -2,65 +2,80 @@ import os
 import sys
 
 import image2matrix as i2m
-from errors import (
-    PathExitError,
-    MatrixSizeError,
-    PathCornerError,
-)
+from errors import MatrixSizeError, PathCornerError, PathExitError
 
-Cell = tuple[int, int]
-Row = Col = tuple[bool, ...]
-Matrix = tuple[Row, ...]
+CellType = tuple[int, int]
+RowType = ColType = tuple[bool, ...]
+MatrixType = tuple[RowType, ...]
 
 
-class Maze:
-    def __init__(self, matrix: Matrix) -> None:
-        self.validate_matrix(matrix)
-
+class Matrix:
+    def __init__(self, matrix: MatrixType):
         self.matrix = matrix
         self.width = len(matrix[0])
         self.height = len(matrix)
-        self.start, self.end = self.exit_cells()
 
-    def row(self, index: int) -> Row:
+    def row(self, index: int) -> RowType:
         i = index if index >= 0 else (index * -1) - 1  # equalize negative index
         if i >= self.height:
             raise IndexError(f"Index out of bounds: {index}")
-        return self.matrix[i]
+        return self.matrix[index]
 
-    def col(self, index: int) -> Col:
+    def col(self, index: int) -> ColType:
         i = index if index >= 0 else (index * -1) - 1  # equalize negative index
         if i >= self.width:
             raise IndexError(f"Index out of bounds: {index}")
-        return tuple([row[i] for row in self.matrix])
+        return tuple([row[index] for row in self.matrix])
 
-    def exit_cells(self) -> list[Cell, Cell]:
+
+class Maze:
+    def __init__(self, matrix: Matrix):
+        self.matrix = matrix
+
+    def solve(self):
+        start_cell, end_cell = self.exit_cells()
+        ...
+
+    def exit_cells(self) -> tuple[CellType, CellType]:
         cells = []
+        north = self.matrix.row(0)
+        east = self.matrix.col(-1)
+        south = self.matrix.row(-1)
+        west = self.matrix.col(0)
 
-        # North
-        for cell in self.row(0):
-            if cell:
-                cells.append((0, self.row(0).index(cell)))
-        # East
-        for cell in self.col(-1):
-            if cell:
-                cells.append((self.col(-1).index(cell), self.width - 1))
-        # South
-        for cell in self.row(-1):
-            if cell:
-                cells.append((self.height - 1, self.row(-1).index(cell)))
-        # West
-        for cell in self.col(0):
-            if cell:
-                cells.append((self.col(0).index(cell), 0))
+        for i in range(self.matrix.width):
+            if north[i]:
+                cells.append((0, i))
+            if south[i]:
+                cells.append((self.matrix.height - 1, i))
 
-        return cells
+        for i in range(self.matrix.height):
+            if east[i]:
+                cells.append((i, self.matrix.width - 1))
+            if west[i]:
+                cells.append((i, 0))
+
+        start_cell, end_cell = cells
+        return start_cell, end_cell
+
+
+class Validator:
+    @staticmethod
+    def validate(matrix: MatrixType):
+        Validator.size(matrix)
+        Validator.corners(matrix)
+        Validator.exit_cell_amt(matrix)
+
+        maze = Maze(Matrix(matrix))
+        Validator.exit_cell_pos(*maze.exit_cells())
 
     @staticmethod
-    def validate_matrix(matrix: Matrix) -> None:
+    def size(matrix: MatrixType):
         if len(matrix) < 3 or any(len(row) < 3 for row in matrix):
             raise MatrixSizeError("Matrix must be at least 3x3")
 
+    @staticmethod
+    def corners(matrix: MatrixType):
         corners = [
             matrix[0][0],
             matrix[0][-1],
@@ -70,6 +85,8 @@ class Maze:
         if any(corners):
             raise PathCornerError("Corner cannot be path")
 
+    @staticmethod
+    def exit_cell_amt(matrix: MatrixType):
         north = [cell for cell in matrix[0]]
         east = [cell for cell in [row[-1] for row in matrix]]
         south = [cell for cell in matrix[-1]]
@@ -78,6 +95,15 @@ class Maze:
         exits = [cell for cell in border_cells if cell]
         if len(exits) != 2:
             raise PathExitError("Expecting exactly 2 exits")
+
+    @staticmethod
+    def exit_cell_pos(start_cell: CellType, end_cell: CellType):
+        # calc delta pos
+        d_y = abs(start_cell[0] - end_cell[0])
+        d_x = abs(start_cell[1] - end_cell[1])
+        # if 1 space apart and on same row/col
+        if d_x == 0 and d_y == 1 or d_y == 0 and d_x == 1:
+            raise PathExitError("Exits must be at least 1 cell apart")
 
 
 if __name__ == "__main__":
@@ -88,7 +114,3 @@ if __name__ == "__main__":
     # generate your own maze img with: https://keesiemeijer.github.io/maze-generator/
     maze_path = i2m.get_maze_path(png=path)
     i2m.print_maze(matrix=maze_path)
-
-    maze = Maze(matrix=maze_path)
-    print(maze.start_cell())
-    print(maze.end_cell())
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
 
 from errors import MatrixSizeError, PathCornerError, PathExitError
-from solve import Maze
+from solve import Maze, Matrix, Validator
 
 
-class TestMaze(unittest.TestCase):
+class TestMatrix(unittest.TestCase):
     def test_matrix_too_small_raises_error(self):
         matrix1 = (
             (0, 1, 0),
@@ -15,8 +15,8 @@ class TestMaze(unittest.TestCase):
             (0, 1, 0),
             (0, 0, 1),
         )
-        self.assertRaises(MatrixSizeError, Maze.validate_matrix, matrix1)
-        self.assertRaises(MatrixSizeError, Maze.validate_matrix, matrix2)
+        self.assertRaises(MatrixSizeError, Validator.size, matrix1)
+        self.assertRaises(MatrixSizeError, Validator.size, matrix2)
 
     def test_matrix_with_corner_path_raises_error(self):
         matrix1 = (
@@ -29,8 +29,8 @@ class TestMaze(unittest.TestCase):
             (0, 0, 0),
             (0, 0, 1),
         )
-        self.assertRaises(PathCornerError, Maze.validate_matrix, matrix1)
-        self.assertRaises(PathCornerError, Maze.validate_matrix, matrix2)
+        self.assertRaises(PathCornerError, Validator.corners, matrix1)
+        self.assertRaises(PathCornerError, Validator.corners, matrix2)
 
     def test_matrix_with_too_many_exits_raises_error(self):
         matrix = (
@@ -38,7 +38,70 @@ class TestMaze(unittest.TestCase):
             (1, 0, 0),
             (0, 1, 0),
         )
-        self.assertRaises(PathExitError, Maze.validate_matrix, matrix)
+        self.assertRaises(PathExitError, Validator.exit_cell_amt, matrix)
+
+    def test_row_out_of_bounds(self):
+        matrix = (
+            (0, 0, 0),
+            (0, 0, 0),
+            (0, 0, 0),
+        )
+        matrix = Matrix(matrix)
+        self.assertRaises(IndexError, matrix.row, 3)
+        self.assertRaises(IndexError, matrix.row, -4)
+
+    def test_row_returns_expected(self):
+        matrix = (
+            (1, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+        )
+        matrix = Matrix(matrix)
+        assert matrix.row(1) == (0, 0, 1)
+        assert matrix.row(-1) == (0, 1, 0)
+
+    def test_col_out_of_bounds(self):
+        matrix = (
+            (0, 0, 0),
+            (0, 0, 0),
+            (0, 0, 0),
+        )
+        matrix = Matrix(matrix)
+        self.assertRaises(IndexError, matrix.row, 3)
+        self.assertRaises(IndexError, matrix.row, -4)
+
+    def test_col_returns_expected(self):
+        matrix = (
+            (1, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+        )
+        matrix = Matrix(matrix)
+        assert matrix.col(1) == (0, 0, 1)
+        assert matrix.col(-1) == (0, 1, 0)
+
+
+class MazeTest(unittest.TestCase):
+    def test_maze_exits_neighbouring_raises_error(self):
+        matrix1 = (
+            (0, 1, 1, 0),
+            (0, 0, 0, 0),
+            (0, 0, 0, 0),
+        )
+        matrix2 = (
+            (0, 0, 0),
+            (1, 0, 0),
+            (1, 0, 0),
+            (0, 0, 0)
+        )
+        maze1 = Maze(Matrix(matrix1))
+        maze2 = Maze(Matrix(matrix2))
+        self.assertRaises(
+            PathExitError, Validator.exit_cell_pos, *maze1.exit_cells()
+        )
+        self.assertRaises(
+            PathExitError, Validator.exit_cell_pos, *maze2.exit_cells()
+        )
 
 
 if __name__ == '__main__':
