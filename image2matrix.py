021b51a2dc18ebf57d40badb33b596a29bbe3e0a
Angelo van Meurs <arcvanmeurs@hotmail.com>
2023-04-12 12:44:57 +0200

    Get matrix from png image

---
+++ image2matrix.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from PIL import Image
+
+
+def get_maze_walls(png: str, inverted: bool = False):
+    img = Image.open(png).convert("RGB")
+    array = np.array(img).tolist()
+
+    wall = [255, 255, 255]
+
+    maze_wall_matrix = []
+    for row in array:
+        maze_wall_matrix.append([cell == wall for cell in row])
+
+    return maze_wall_matrix
+
+
+def print_maze(matrix, ch='H', sep=' '):
+    for row in matrix:
+        for px in row:
+            s = ch + sep if px else ' ' + sep
+            print(s, end='')
+        print()
