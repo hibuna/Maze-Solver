85189eb84df9422458b641da3b300af71160afab
Angelo van Meurs <arcvanmeurs@hotmail.com>
2023-04-19 15:32:12 +0200

    Add gradient to png export

--- matrix2image.py
+++ matrix2image.py
@@ -7,10 +7,17 @@ def create_png(path: str, size: tuple[int, int], matrix: list, solution: list):
     draw = ImageDraw.Draw(png, mode="RGB")
     for cell in matrix:
         draw.point(xy=cell, fill=(255, 255, 255))
-    for cell in solution:
-        draw.point(xy=cell, fill=(255, 0, 0))
 
-    png = png.rotate(angle=270)  # turn once CCW to equalize row/col to xy
+    red_base, green_base = 0, 255
+    gradient_increment = 255 / len(solution)
+    for i, cell in enumerate(solution):
+        red = red_base + round(i * gradient_increment)
+        green = green_base - round(i * gradient_increment)
+        red = 255 if red > 255 else red
+        green = 0 if green < 0 else green
+        draw.point(xy=cell, fill=(red, green, 0))
+
+    png = png.rotate(angle=270)  # row/col != xy
     png = ImageOps.mirror(png)
 
     png.save(path)
