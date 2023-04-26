dcb578a43a8346490db075048a93529167d717cc
Angelo van Meurs <arcvanmeurs@hotmail.com>
2023-04-26 15:26:42 +0200

    Format

--- matrix2image.py
+++ matrix2image.py
@@ -3,7 +3,6 @@ from PIL import Image, ImageDraw, ImageOps
 
 def create_png(path: str, size: tuple[int, int], matrix: list, solution: list):
     png = Image.new(mode="RGB", size=size, color=(0, 0, 0))
-
     draw = ImageDraw.Draw(png, mode="RGB")
     for cell in matrix:
         draw.point(xy=cell, fill=(255, 255, 255))
