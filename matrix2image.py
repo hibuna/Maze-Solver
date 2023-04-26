6693c507ebd033d2bfcd0744f4a9f52efd8291c8
Angelo van Meurs <arcvanmeurs@hotmail.com>
2023-04-26 15:45:25 +0200

    Reverse color gradient

--- matrix2image.py
+++ matrix2image.py
@@ -7,13 +7,13 @@ def create_png(path: str, size: tuple[int, int], matrix: list, solution: list):
     for cell in matrix:
         draw.point(xy=cell, fill=(255, 255, 255))
 
-    red_base, green_base = 0, 255
+    red_base, green_base = 255, 0
     gradient_increment = 255 / len(solution)
     for i, cell in enumerate(solution):
-        red = red_base + round(i * gradient_increment)
-        green = green_base - round(i * gradient_increment)
-        red = 255 if red > 255 else red
-        green = 0 if green < 0 else green
+        red = red_base - round(i * gradient_increment)
+        green = green_base + round(i * gradient_increment)
+        red = 0 if red < 0 else red
+        green = 255 if green > 255 else green
         draw.point(xy=cell, fill=(red, green, 0))
 
     png = png.rotate(angle=270)  # row/col != xy
